5f638a2729e32434e08137030c0dd90a6a53194f
it62070180 <56427172+it62070180@users.noreply.github.com>
2022-03-15 01:50:01 +0700

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,18 +1,19 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { Nav } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 
 const Navber = () => {
     return(
     <Navbar style={{ backgroundColor: "#061314" }} variant="dark">
-        <Container>
-        <Navbar.Brand href="/">SIWAX</Navbar.Brand>
-        <Nav className="me-auto">
-        <Nav.Link href="/">HomePage</Nav.Link>
-        <Nav.Link href="/category">Category</Nav.Link>
-        <Nav.Link href="/Author">Author</Nav.Link>
-        </Nav>
+        <Container >
+            <Navbar.Brand to="/">SIWAX</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Link to="/"  className='pe-1' style={{textDecoration:'none',color:'white'}} >HomePage</Link>
+                    <Link to="/category"  className='pe-1' style={{textDecoration:'none',color:'white'}} >Category</Link>
+                    <Link to="/Author"  className='pe-1' style={{textDecoration:'none',color:'white'}} >Author</Link>
+                </Nav>
         </Container>
     </Navbar>
  );
